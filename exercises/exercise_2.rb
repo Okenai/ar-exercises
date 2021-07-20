0ddcffe0f477d08b73501e00daa93eaead890a2e
Lana <kassembayeva@gmail.com>
2021-07-20 23:12:54 +0000

    excercise 2 is editted

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,10 +5,8 @@ puts "Exercise 2"
 
 
 # Your code goes here ...
-store1 = Store.find_by(id: 1)
-@store1 = store1
-store2 = Store.where(id: 2)
-@store2 = store2
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
 
 puts "store 1 before: #{@store1.name}" 
 
