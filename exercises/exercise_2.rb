771de4475d746e2b10b14d22c5af09fef4a8f8b6
Lana <kassembayeva@gmail.com>
2021-07-20 20:08:08 +0000

    exercise 2 is complete

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -2,6 +2,15 @@ require_relative '../setup'
 require_relative './exercise_1'
 
 puts "Exercise 2"
-puts "----------"
+
 
 # Your code goes here ...
+store1 = Store.find_by(id: 1)
+@store1 = store1
+store2 = Store.where(id: 2)
+@store2 = store2
+
+puts "store 1 before: #{@store1.name}" 
+
+@store1.name = "First"
+puts "store 1 after: #{@store1.name}" 
